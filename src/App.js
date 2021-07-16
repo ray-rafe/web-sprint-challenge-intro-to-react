447a83e620a58388fba71f5ff7136071b116484d
Rory Roberts <82188608+Rory-Ray@users.noreply.github.com>
2021-07-16 12:46:20 -0600

    MVP

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Character from "./components/Character";
+import styled from "styled-components";
+
+const StyledApp = styled.div`
+  padding: 5px;
+`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  const [data, setData] = useState([]);
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios
+      .get("https://swapi.py4e.com/api/people/")
+
+      .then((res) => {
+        console.log(res);
+        setData(res.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Starwars</h1>
+      <StyledApp>
+        <Character data={data} />
+      </StyledApp>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,44 @@
-// Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const Cards = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: space-around;
+  align-content: stretch;
+  padding: 3rem;
+`;
+
+const Card = styled.div`
+  border: 5px solid white;
+  border-radius: 4px;
+  padding: 3rem;
+  background-color: black;
+  opacity: 70%;
+  color: beige;
+  margin-left: auto;
+  margin-right: auto;
+  width: 8em;
+`;
+
+const Character = (props) => {
+  let { data } = props;
+
+  return (
+    <div>
+      {data.map((starWars) => {
+        return (
+          <Cards>
+            <Card>
+              <h3>Name: {starWars.name}</h3>
+              <h3>Gender: {starWars.gender}</h3>
+              <h3>Birth Year: {starWars.birth_year}</h3>
+            </Card>
+          </Cards>
+        );
+      })}
+    </div>
+  );
+};
+export default Character;
